631cc288ee66b7fcc64311e28ce03fe16f01e38c
sebastianrp-lab <sebastian.rodriguezp@urosario.edu.co>
2025-02-01 12:55:33 -0500

    Primer lab

--- app.py
+++ app.py
@@ -45,5 +45,66 @@ def patch_task(task_id):
     save_data()
     return jsonify(task)
 
+
+#Creacion de tarea
+@app.route('/tasks/create_task', methods=['POST'])
+def create_task():
+    data = request.json
+    tasks.append(data)
+    save_data()
+    return jsonify(tasks)
+  
+
+#Creacion de categoria
+@app.route('/categories/create_category', methods=['POST'])
+def create_category():
+    data = request.json
+    categories.append(data)
+    save_data()
+    return jsonify(categories)
+
+#Actualizar tarea
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = next((task for task in tasks if task["id"] == task_id), None)
+    if task is None:
+        abort(404, description="Tarea no encontrada")
+    data = request.json
+    task.update(data)
+    save_data()
+    return jsonify(tasks)
+
+#Actualizar categoria
+@app.route('/categories/<int:category_id>', methods=['PUT'])
+def update_category(category_id):
+    category = next((category for category in categories if category["id"] == category_id), None)
+    if category is None:
+        abort(404, description="Categoria no encontrada")
+    data = request.json
+    category.update(data)
+    save_data()
+    return jsonify(categories)
+
+#Eliminar tarea
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = next((task for task in tasks if task["id"] == task_id), None)
+    if task is None:
+        abort(404, description="Tarea no encontrada")
+    tasks.remove(task)
+    save_data()
+    return jsonify(tasks)
+
+#Eliminar categoria
+@app.route('/categories/<int:category_id>', methods=['DELETE'])
+def delete_category(category_id):
+    category = next((category for category in categories if category["id"] == category_id), None)
+    if category is None:
+        abort(404, description="Tarea no encontrada")
+    categories.remove(category)
+    save_data()
+    return jsonify(categories)
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
